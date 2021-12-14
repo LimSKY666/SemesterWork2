e5ffa318d4c808339249be0ff2f399c82cf70bee
Semen Sokolov <simionsimion@yandex.ru>
2021-12-14 18:42:50 +0300

    Training mode (need to fix some), start controller. fxml empty files

--- src/ru/kpfu/itis/sokolov/controller/SceneLoader.java
+++ src/ru/kpfu/itis/sokolov/controller/SceneLoader.java
@@ -1,5 +1,23 @@
 package ru.kpfu.itis.sokolov.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import ru.kpfu.itis.sokolov.model.Player;
+
+import java.io.IOException;
+
 public class SceneLoader {
 
+    public static void showWaitingController(Stage primaryStage, int PORT) throws IOException {
+        FXMLLoader loader = new FXMLLoader(SceneLoader.class.getResource("../view/waiting.fxml"));
+        Parent root = loader.load();
+
+        WaitingController waitingController = (WaitingController) loader.getController();
+        waitingController.init(primaryStage, PORT);
+
+        primaryStage.setTitle("ATARI PONG");
+        primaryStage.getScene().setRoot(root);
+        primaryStage.show();
+    }
 }
